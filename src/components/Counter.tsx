aadf1f7c1cb4ab698395ec9d158fdf65f9084553
aileenhuang <aileenhuang@tencent.com>
2024-05-17 17:47:42 +0800

    feat: dark mode only

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -7,7 +7,7 @@ function CounterButton() {
 
   return (
     <div class="flex gap-4 items-center">
-      <button onClick={increment} class="px-3 py-1 border border-black/25 dark:border-white/25 hover:bg-black/5 dark:hover:bg-white/15 blend">
+      <button onClick={increment} class="px-3 py-1 border border-white/25 hover:bg-white/15 blend">
         Increment
       </button>
       <div>
